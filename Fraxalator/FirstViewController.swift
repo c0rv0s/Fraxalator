a18300c823fc8d93f27d9a6e2894fbe4693ea7c6
c0rv0s <inathan.m@gmail.com>
2016-06-09 12:07:37 -0700

    Initial Commit

---
+++ Fraxalator/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Fraxalator
+//
+//  Created by Nathan Mueller on 6/9/16.
+//  Copyright © 2016 OneNathan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
