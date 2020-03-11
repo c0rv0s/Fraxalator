d664557408a40669a058f0b8345b153b5f6e68c7
Austin Conlon <austinconlon@icloud.com>
2020-03-11 03:35:02 -0700

    style list as a grouped list

--- Fraxalator/MainView.swift
+++ Fraxalator/MainView.swift
@@ -15,26 +15,31 @@ struct MainView: View {
     var body: some View {
         NavigationView {
             List {
+                Section {
+                    ForEach(0..<self.score.letters.count) {
+                        ItemRow(letter: self.score.letters[$0], score: self.$score)
+                    }
+                    ItemRow(letter: "S", score: self.$score)
+                }
                 
-                ForEach(0..<self.score.letters.count) {
-                    ItemRow(letter: self.score.letters[$0], score: self.$score)
-                }
-                ItemRow(letter: "S", score: self.$score)
-                Button(action: {
-                    self.score.generateScore()
-                    self.showingScore.toggle()
-                    self.score.playScore()
-                }) {
-                    Text("Create")
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(Color.blue)
-                }
-                .sheet(isPresented: $showingScore) {
-                    Text(self.score.fractalScore)
-                        .padding()
+                Section {
+                    Button(action: {
+                        self.score.generateScore()
+                        self.showingScore.toggle()
+                        self.score.playScore()
+                    }) {
+                        Text("Create")
+                            .frame(maxWidth: .infinity)
+                            .foregroundColor(Color.blue)
+                    }
+                    .sheet(isPresented: $showingScore) {
+                        Text(self.score.fractalScore)
+                            .padding()
+                    }
                 }
             }
             .navigationBarTitle(Text("Fractal Rules"))
+            .listStyle(GroupedListStyle())
         }
     }
 }
